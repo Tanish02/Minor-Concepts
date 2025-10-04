8b6d6c93beb734c393517b406815b4607e7122ee
Tanish Sharma <tanishsharma080@gmail.com>
2025-10-04 21:04:14 +0530

    add async function

--- App.js
+++ App.js
@@ -3,15 +3,41 @@
 // Synchronous code --> line by line execution
 // if one line takes time, everything waits for it to finish
 // then execute the next line  --> Also Known Singal Threaded JS
-function main() {
-  console.log("1. Start");
-  console.log("2. Start");
-  console.log("3. Start");
-  console.log("4. Start");
-}
-main();
+// function main() {
+//   console.log("1. Start");
+//   console.log("2. Start");
+//   console.log("3. Start");
+//   console.log("4. Start");
+// }
+// main();
 
 // code Example -->
+
+// function checkInventory() {
+//   console.log("Checking Inventory --->");
+// }
+
+// function createOrder() {
+//   console.log("Creating Order --->");
+// }
+
+// function chargePayment() {
+//   console.log("Charging Payment--->");
+// }
+
+// function sendInvoice() {
+//   console.log("Sending Invoice --->");
+// }
+
+// function main() {
+//   checkInventory();
+//   createOrder();
+//   chargePayment();
+//   sendInvoice();
+// }
+// main();
+
+// Asynchronous code -->
 
 function checkInventory() {
   console.log("Checking Inventory --->");
